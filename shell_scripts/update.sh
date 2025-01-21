b583874d738002df36ba7d0e43fae743a5c06e98
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-21 09:51:28 -0500

    Update shell script to include a test comment for Home Assistant YAML refresh service

--- shell_scripts/update.sh
+++ shell_scripts/update.sh
@@ -5,7 +5,7 @@ cd /config
 # Update the submodules
 git submodule update --remote --recursive
 
-# Call the Home Assistant "Refresh All YAML" service
+# Call the Home Assistant "Refresh All YAML" service  TEST
 curl -X POST \
     -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NDdjN2U3ZmJkNzM0YzFjOGI2MWYyYTM2ZGE5MjU4NyIsImlhdCI6MTczNzQ2ODM0MywiZXhwIjoyMDUyODI4MzQzfQ.rjn6HGsvGbsB3lEMol0B1jGdxDuUZ56bE3m3kyhDo2I" \
     -H "Content-Type: application/json" \
