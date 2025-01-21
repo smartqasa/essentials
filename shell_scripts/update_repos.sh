aecd665645fcfff2ff7170dc0df1c3cf2d3549a9
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-21 15:10:03 -0500

    Remove commented-out curl command for Home Assistant YAML refresh in update_repos.sh

--- shell_scripts/update_repos.sh
+++ shell_scripts/update_repos.sh
@@ -5,8 +5,8 @@ cd /config
 # Update the submodules
 git submodule update --remote --recursive --force
 
-# Call the Home Assistant "Refresh All YAML" service  TEST 3
-curl -X POST \
-    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NDdjN2U3ZmJkNzM0YzFjOGI2MWYyYTM2ZGE5MjU4NyIsImlhdCI6MTczNzQ2ODM0MywiZXhwIjoyMDUyODI4MzQzfQ.rjn6HGsvGbsB3lEMol0B1jGdxDuUZ56bE3m3kyhDo2I" \
-    -H "Content-Type: application/json" \
-    http://192.168.75.10:10075/api/services/homeassistant/reload_core_config
+# Call the Home Assistant "Refresh All YAML" service
+#curl -X POST \
+#    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NDdjN2U3ZmJkNzM0YzFjOGI2MWYyYTM2ZGE5MjU4NyIsImlhdCI6MTczNzQ2ODM0MywiZXhwIjoyMDUyODI4MzQzfQ.rjn6HGsvGbsB3lEMol0B1jGdxDuUZ56bE3m3kyhDo2I" \
+#    -H "Content-Type: application/json" \
+#    http://192.168.75.10:10075/api/services/homeassistant/reload_core_config
